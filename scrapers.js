3c1d451647abbc26c6f12f3189d45e1dd2c87399
Thea <thea.mushambadze@gmail.com>
2020-08-15 02:13:53 +0400

    first commit

---
+++ scrapers.js
@@ -0,0 +1,27 @@
+const puppeteer = require("puppeteer");
+
+async function scrapeProduct(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const [el] = await page.$x('//*[@id="coverImage"]');
+    const src = await el.getProperty('src');
+    const imgUrl = await src.jsonValue();
+
+    const [el2] = await page.$x('//*[@id="bookTitle"]');
+    const txt = await el2.getProperty('textContent');
+    const title = await txt.jsonValue();
+
+    const [el3] = await page.$x('//*[@id="bookAuthors"]');
+    const txt2 = await el3.getProperty('textContent');
+    const writer = await txt2.jsonValue();
+
+
+    console.log({ imgUrl, title, writer });
+
+    browser.close();
+
+}
+scrapeProduct("https://www.goodreads.com/book/show/26828169-the-neapolitan-novels");
+scrapeProduct("https://www.goodreads.com/book/show/26828169-the-neapolitan-novels");
